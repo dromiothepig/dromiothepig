8cb7612c3ca63c4497bf4681ab0c2b17cba3ffb9
proPianist1124 <phantomforces11242009@gmail.com>
2025-08-24 17:39:10 -0700

    updated meta tags and unfinished blog post page

--- src/routes/blog/[post]/+page.svelte
+++ src/routes/blog/[post]/+page.svelte
@@ -1,3 +1,12 @@
+<script>
+  import { Button } from "$lib/components/ui/button/index.js";
+</script>
+
 <title>blog post / dromiothepig</title>
 
-hello!
+<main>
+  <b class="block text-3xl mb-5 text-destructive">UNFINISHED WIP</b>
+  <a href="/">
+    <Button>Go Back</Button>
+  </a>
+</main>
